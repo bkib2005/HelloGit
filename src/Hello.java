23912be2027692e80a6b4c09b4cd2f7b19af4da7
setht <setht@s-obert.unl.edu>
2024-08-29 13:04:15 -0500

    added my major and name

--- src/Hello.java
+++ src/Hello.java
@@ -3,5 +3,8 @@ public class Hello {
 	public static void main(String args[]) {
 		System.out.println("Barrett Kiburz");
 		System.out.println("Computer Science");
+		
+		System.out.println("My name is Seth Obert");
+		System.out.println("My major is computer engineering");
 	}
 }
